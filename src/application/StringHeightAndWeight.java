3664321e41aa2020f5afb2b0383bbcc093421438
AlinaMansuri <alina.mansuri@ucalgary.ca>
2022-12-10 06:47:19 +0000

    broken into smaller purposeful functions apart from jogging and running

---
+++ src/application/StringHeightAndWeight.java
@@ -0,0 +1,17 @@
+package application;
+
+public class StringHeightAndWeight {
+	String heightValue;
+	String weightValue;
+	public StringHeightAndWeight(String Height,String Weight) {
+		this.heightValue = Height;
+		this.weightValue = Weight;
+	}
+	public String getHeight() {
+		return this.heightValue;
+	}
+	
+	public String getWeight() {
+		return this.weightValue;
+	}
+}
