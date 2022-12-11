c3f0867c2fcd18184837ceae058ae3266c1a0b62
AlinaMansuri <alina.mansuri@ucalgary.ca>
2022-12-11 01:14:39 +0000

    please pull from breaktopurposefulfunctions

--- src/application/Exercise.java
+++ src/application/Exercise.java
@@ -1,34 +1,3 @@
-package application;
-
-/**
- * This class takes the time spent doing an exercise and the user's weight 
- * to calculate the calories burned during the workout. METenergy is provided
- * internally from the authors. Has three subclasses
- * 
- * @author Alina, Kaleigh, Marzia
- *
- */
-public abstract class Exercise { 
-	/**
-	 * attributes data to the Exercise class using instance variables,
-	 * and their functions are described above
-	 */
-	double weight;
-	double duration;
-/**
- * A constructor used to initialize the instance variables
- * @param typeOfExercise
- * @param pounds
- */
-	Exercise(double typeOfExercise, double pounds) {
-		weight = pounds;
-		duration = typeOfExercise;
-	}
-	abstract double getCalorieCalc();
-}
-
-
-
 
 
 
