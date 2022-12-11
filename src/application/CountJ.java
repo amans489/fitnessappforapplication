280f0812d4ae86e2da4db4fdafd0b9f9331004a7
AlinaMansuri <alina.mansuri@ucalgary.ca>
2022-12-11 00:43:20 +0000

    had to do this manually because could not resolve merge conflicts

--- src/application/CountJ.java
+++ src/application/CountJ.java
@@ -1,26 +1,48 @@
 package application;
 
-public class CountJ {
+/**
+ * This is a subclass of Exercise which takes the parameters of the exercise class
+ * and calculates the amount of calories burned when jogging based on duration.
+ * @author Alina, Kaleigh, Marzia
+ *
+ */
+public class CountJ extends Exercise {
+	/**
+	 * instance variable for the amount of energy used when jogging, set
+	 * at a constant
+	 */
+	private int METenergyW = 8;
 	
-	double weightJ;
-	int METenergyJ;
-	double durationJ;
+	/**
+	 * Constructor that sets the instance variables, derived from the parent class
+	 * of Exercise 
+	 * @param eneegy, the MET is the metabolic equivalent of task, used to determine 
+	 * how much energy is used when jogging 
+	 * @param joggingDuration, time spent jogging
+	 * @param pounds, weight of the user in pounds
+	 */
+	
+	public CountJ(double jog,double pounds) {
+		super(jog, pounds);
+	}
+	
+	/**
+	 * A gretter method to return the Metabolic Energy constant
+	 * @return an integer value of the MET
+	 */
+	public int getMETenergyW() {
+		return METenergyW;
+	}
+	/**
+	 * A method that gets the calories burned calculated when
+	 * jogging overriding the parent class of Exercise
+	 */
+	
+	@Override 
+	double getCalorieCalc() {
+		return METenergyW*super.weight*super.duration/(200);
+}
 
 
 
-	 CountJ(int energy, double jog, double pounds){
-		 METenergyJ = energy;
-		 weightJ = pounds;
-		 durationJ = jog;
-	 }
-
-	 
-	// public double calorieCalc (int MET, double distanceRan, double bodyW) {
-
-	// double caloriesBurned = (MET*distanceRan*bodyW)/200;
-	 //return caloriesBurned;
-	 
-	 double getCalorieCalc() {
-	return (METenergyJ*weightJ*durationJ)/200;
-	 }
 }
--- src/application/CountW.java
+++ src/application/CountW.java
@@ -1,26 +1,48 @@
 package application;
 
-public class CountW {
-	double weightW;
-	int METenergyW;
-	double durationW;
+/**
+ * This is a subclass of Exercise which takes the parameters of the exercise class
+ * and calculates the amount of calories burned when running based on duration.
+ * @author Alina, Kaleigh, Marzia
+ *
+ */
+public class CountW extends Exercise {
+	/**
+	 * instance variable for the amount of energy used when running, set
+	 * at a constant
+	 */
+	private int METenergyW = 7;
+	
+	/**
+	 * Constructor that sets the instance variables, derived from the parent class
+	 * of Exercise 
+	 * @param eneegy, the MET is the metabolic equivalent of task, used to determine 
+	 * how much energy is used when walking
+	 * @param walkingDuration, time spent walking
+	 * @param pounds, weight of the user in pounds
+	 */
+	
+	public CountW(double jog,double pounds) {
+		super(jog, pounds);
+	}
+	
+	/**
+	 * A getter method to return the Metabolic Energy constant
+	 * @return an integer value of the MET
+	 */
+	public int getMETenergyW() {
+		return METenergyW;
+	}
+	/**
+	 * A method that gets the calories burned calculated when
+	 * walking overriding the parent class of Exercise
+	 */
+	
+	@Override 
+	double getCalorieCalc() {
+		return METenergyW*super.weight*super.duration/(200);
+}
 
 
-
-	 CountW(int energy, double jog, double pounds){
-		 METenergyW = energy;
-		 weightW = pounds;
-		 durationW = jog;
-	 }
-
-	 
-	// public double calorieCalc (int MET, double distanceRan, double bodyW) {
-
-	// double caloriesBurned = (MET*distanceRan*bodyW)/200;
-	 //return caloriesBurned;
-	 
-	 double getCalorieCalc() {
-	return (METenergyW*weightW*durationW)/200;
-	 }
 
 }
